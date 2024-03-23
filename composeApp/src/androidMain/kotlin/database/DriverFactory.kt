101055758678e29fc149ac5c7cdc19e121e3d558
antevg <antevg@gmail.com>
2024-03-23 20:22:15 +0300

    error androidContext()

--- composeApp/src/androidMain/kotlin/database/DriverFactory.kt
+++ composeApp/src/androidMain/kotlin/database/DriverFactory.kt
@@ -4,7 +4,7 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.eacipher.doggify.db.DogifyDatabase
 import org.koin.core.scope.Scope
-import org.koin.android.ext.koin.androidContext
+
 
 internal actual fun Scope.createDriver(databaseName: String): SqlDriver =
     AndroidSqliteDriver(DogifyDatabase.Schema, androidContext(), databaseName)
